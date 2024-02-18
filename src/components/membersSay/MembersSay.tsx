249b715537ad4f165e2fe39bb5df3ddebd78cde0
VictorMFL <vm859222@gmail.com>
2024-02-18 20:58:02 -0300

    style: <MembersSay />  responsive

--- src/components/membersSay/MembersSay.tsx
+++ src/components/membersSay/MembersSay.tsx
@@ -12,12 +12,12 @@ const MembersSay = () => {
 
   return (
     <div className="w-full bg-newBlue flex flex-col items-center justify-center py-[60px]">
-      <h1 className="font-normal text-4xl text-white" data-aos="zoom-in">
+      <h1 className="font-normal text-4xl cel:text-2xl cel:text-center text-white" data-aos="zoom-in">
         Veja o que nossos membros têm a dizer:
       </h1>
 
-      <div className="w-full flex items-center justify-center px-28 mt-20">
-        <div className="w-full grid grid-cols-3 gap-12 place-items-center justify-center">
+      <div className="w-full flex items-center justify-center px-28 cel:px-8 mt-20 cel:mt-14">
+        <div className="w-full grid grid-cols-3 tablet:grid-cols-[1fr] cel:grid-cols-1 gap-12 cel:gap-6 place-items-center justify-center">
           <Comment
             comment="Texto de amostra. Aqui que ficará o comentário dos membros."
             name="Victor Manoel"
