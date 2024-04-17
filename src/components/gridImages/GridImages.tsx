18e2814cf3c063451c216cf230ef393e5f1a03aa
VictorMFL <vm859222@gmail.com>
2024-04-17 11:41:08 -0300

    style: <GridImages /> responsive

--- src/components/gridImages/GridImages.tsx
+++ src/components/gridImages/GridImages.tsx
@@ -10,11 +10,11 @@ const GridImages = () => {
   }, []);
 
   return (
-    <div className="bg-newBlack h-[374px] flex items-center justify-center gap-[26px]">
-      <div className="bg-exemple1 relative top-16" data-aos="fade-up"></div>
-      <div className="bg-exemple2 relative top-40" data-aos="fade-down"></div>
-      <div className="bg-exemple3 relative top-16" data-aos="fade-up"></div>
-      <div className="bg-exemple4 relative top-40" data-aos="fade-down"></div>
+    <div className="bg-newBlack h-[374px] tablet:h-auto tablet:pb-6 flex tablet:flex-col items-center justify-center gap-[26px]">
+      <div className="bg-exemple1 relative top-16 tablet:top-4" data-aos="fade-up"></div>
+      <div className="bg-exemple2 relative top-40 tablet:top-4" data-aos="fade-down"></div>
+      <div className="bg-exemple3 relative top-16 tablet:top-4" data-aos="fade-up"></div>
+      <div className="bg-exemple4 relative top-40 tablet:top-4" data-aos="fade-down"></div>
     </div>
   );
 };
